423e4616438eeafdc6719523ec69eb1dc7d122eb
Aaron Schlesinger <aschlesinger@deis.com>
2016-04-06 15:44:07 -0700

    fix(handlers/k8s_test.go): add test for findExpiredLease

---
+++ handlers/k8s_test.go
@@ -0,0 +1,55 @@
+package handlers
+
+import (
+	"testing"
+	"time"
+
+	"github.com/arschles/assert"
+	"github.com/deis/k8s-claimer/leases"
+	"github.com/deis/k8s-claimer/testutil"
+	"github.com/pborman/uuid"
+)
+
+func TestFindExpiredLease(t *testing.T) {
+	clusterNames := testutil.GetClusterNames()
+	expClusterIdx := -1
+	uuids := make([]uuid.UUID, len(clusterNames))
+	rawAnnotations := testutil.GetRawAnnotations(
+		clusterNames,
+		leases.TimeFormat,
+		func(i int) time.Time {
+			if expClusterIdx == -1 {
+				expClusterIdx = i
+				return time.Now().Add(-1 * time.Second)
+			}
+			return time.Now().Add(1 * time.Hour)
+		},
+		func(i int) uuid.UUID {
+			ret := uuid.NewUUID()
+			uuids[i] = ret
+			return ret
+		},
+	)
+	expClusterName := clusterNames[expClusterIdx]
+	expClusterUUID := uuids[expClusterIdx]
+	leaseMap, err := leases.ParseMapFromAnnotations(rawAnnotations)
+	assert.NoErr(t, err)
+	expLease, found := leaseMap.LeaseByClusterName(expClusterName)
+	assert.True(t, found, "expired lease for cluster %s not found", expClusterName)
+	expired, err := findExpiredLease(leaseMap)
+	assert.NoErr(t, err)
+	assert.Equal(t, expired.Lease.ClusterName, expLease.ClusterName, "cluster name")
+	assert.Equal(t, expired.UUID.String(), expClusterUUID.String(), "lease UUID")
+
+	rawAnnotations = testutil.GetRawAnnotations(
+		clusterNames,
+		leases.TimeFormat,
+		func(int) time.Time { return time.Now().Add(1 * time.Hour) },
+		testutil.DefaultUUIDFunc,
+	)
+	leaseMap, err = leases.ParseMapFromAnnotations(rawAnnotations)
+	assert.NoErr(t, err)
+	expired, err = findExpiredLease(leaseMap)
+	assert.True(t, expired == nil, "non-nil expired lease returned when non were expired")
+	assert.Err(t, errNoExpiredLeases, err)
+}
