6aaec9b2c81a6e209255e699d270d566f03785e1
Aaron Schlesinger <aschlesinger@deis.com>
2016-04-07 12:36:52 -0700

    fix(handlers/delete_lease_test.go): add more tests for the delete lease handler

--- handlers/delete_lease_test.go
+++ handlers/delete_lease_test.go
@@ -6,13 +6,45 @@ import (
 	"testing"
 
 	"github.com/arschles/assert"
+	"github.com/pborman/uuid"
+	"k8s.io/kubernetes/pkg/api"
 )
 
-func TestDeleteLease(t *testing.T) {
-	hdl := DeleteLease(nil)
+func TestDeleteLeaseNoToken(t *testing.T) {
+	getterUpdater := newFakeServiceGetterUpdater(nil, nil, nil, nil)
+	hdl := DeleteLease(getterUpdater, "claimer")
 	req, err := http.NewRequest("DELETE", "/lease", nil)
 	assert.NoErr(t, err)
 	res := httptest.NewRecorder()
 	hdl.ServeHTTP(res, req)
-	assert.Equal(t, res.Code, http.StatusOK, "response code")
+	assert.Equal(t, res.Code, http.StatusBadRequest, "response code")
+}
+
+func TestDeleteLeaseInvalidLeaseToken(t *testing.T) {
+	getterUpdater := newFakeServiceGetterUpdater(nil, nil, nil, nil)
+	hdl := DeleteLease(getterUpdater, "claimer")
+	req, err := http.NewRequest("DELETE", "/lease/abcd", nil)
+	assert.NoErr(t, err)
+	res := httptest.NewRecorder()
+	hdl.ServeHTTP(res, req)
+	assert.Equal(t, res.Code, http.StatusBadRequest, "response code")
+}
+
+func TestDeleteLeaseInvalidAnnotations(t *testing.T) {
+	t.Skip("TODO")
+}
+
+func TestDeleteLeaseNoSuchLease(t *testing.T) {
+	getterUpdater := newFakeServiceGetterUpdater(
+		&api.Service{ObjectMeta: api.ObjectMeta{Annotations: map[string]string{}}},
+		nil,
+		nil,
+		nil,
+	)
+	hdl := DeleteLease(getterUpdater, "claimer")
+	req, err := http.NewRequest("DELETE", "/lease/"+uuid.New(), nil)
+	assert.NoErr(t, err)
+	res := httptest.NewRecorder()
+	hdl.ServeHTTP(res, req)
+	assert.Equal(t, res.Code, http.StatusConflict, "response code")
 }
