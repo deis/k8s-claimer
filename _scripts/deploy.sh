67b3bb83e9994aed63d8d4a9e886ad2d55d86b8b
Aaron Schlesinger <aschlesinger@deis.com>
2016-04-12 15:32:49 -0700

    fix(_deploy.sh): give the makefile the right location of the deis CLI

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -14,4 +14,4 @@ DEIS_REGISTRY=quay.io/ make -C .. docker-build docker-push
 # download deis CLI & deploy to deis
 curl -sSL http://deis.io/deis-cli/install-v2.sh | bash
 ./deis login --username=$DEIS_USERNAME --password=$DEIS_PASSWORD deis.k8s-claimer-e2e.deis.com
-DEIS_BINARY_NAME=./deis DEIS_APP_NAME=k8s-claimer-e2e make -C .. deploy-to-deis
+DEIS_BINARY_NAME=./_scripts/deis DEIS_APP_NAME=k8s-claimer-e2e make -C .. deploy-to-deis
