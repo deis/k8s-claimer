61c08641174a217d332c7abe2ed504973ce9207a
Jonathan Chauncey <jchauncey@deis.com>
2016-09-01 15:26:18 -0400

    fix(travis): Move to new champagne cluster

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -13,5 +13,5 @@ DEIS_REGISTRY=quay.io/ make -C .. docker-build docker-push
 
 # download deis CLI & deploy to deis
 curl -sSL http://deis.io/deis-cli/install-v2.sh | bash
-./deis login --username=$DEIS_USERNAME --password=$DEIS_PASSWORD deis.k8s-claimer-e2e.deis.com
-DEIS_BINARY_NAME=./_scripts/deis DEIS_APP_NAME=k8s-claimer-e2e make -C .. deploy-to-deis
+./deis login --username=$DEIS_USERNAME --password=$DEIS_PASSWORD deis.champagne.deis.com
+DEIS_BINARY_NAME=./_scripts/deis DEIS_APP_NAME=k8s-claimer make -C .. deploy-to-deis
