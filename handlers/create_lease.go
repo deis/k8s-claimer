ce61cc8dd29f8a3718f78572cacb5ba98e9df994
Jonathan Chauncey <jchauncey@deis.com>
2016-09-02 14:02:08 -0400

    chore(create/delete lease): Add logging statements

--- handlers/create_lease.go
+++ handlers/create_lease.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 	"time"
 
@@ -86,18 +87,21 @@ func CreateLease(
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		req := new(api.CreateLeaseReq)
 		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			log.Printf("error decoding JSON (%s)", err)
 			htp.Error(w, http.StatusBadRequest, "error decoding JSON (%s)", err)
 			return
 		}
 
 		clusterMap, svc, err := getSvcsAndClusters(clusterLister, services, gCloudProjID, gCloudZone, k8sServiceName)
 		if err != nil {
+			log.Printf("error listing GKE clusters or talking to the k8s API (%s)", err)
 			htp.Error(w, http.StatusInternalServerError, "error listing GKE clusters or talking to the k8s API (%s)", err)
 			return
 		}
 
 		leaseMap, err := leases.ParseMapFromAnnotations(svc.Annotations)
 		if err != nil {
+			log.Printf("error parsing leases from Kubernetes annotations (%s)", err)
 			htp.Error(w, http.StatusInternalServerError, "error parsing leases from Kubernetes annotations (%s)", err)
 			return
 		}
@@ -106,12 +110,15 @@ func CreateLease(
 		if err != nil {
 			switch e := err.(type) {
 			case errNoAvailableOrExpiredClustersFound:
+				log.Printf("no available clusters found")
 				htp.Error(w, http.StatusConflict, "no available clusters found")
 				return
 			case errExpiredLeaseGKEMissing:
+				log.Printf("cluster %s has an expired lease but doesn't exist in GKE", e.clusterName)
 				htp.Error(w, http.StatusInternalServerError, "cluster %s has an expired lease but doesn't exist in GKE", e.clusterName)
 				return
 			default:
+				log.Printf("unknown error %s", e.Error())
 				htp.Error(w, http.StatusInternalServerError, "unknown error %s", e.Error())
 				return
 			}
@@ -120,18 +127,14 @@ func CreateLease(
 		newToken := uuid.NewUUID()
 		kubeConfig, err := createKubeConfigFromCluster(availableCluster)
 		if err != nil {
-			htp.Error(
-				w,
-				http.StatusInternalServerError,
-				"error creating kubeconfig file for cluster %s (%s)",
-				availableCluster.Name,
-				err,
-			)
+			log.Printf("error creating kubeconfig file for cluster %s (%s)", availableCluster.Name, err)
+			htp.Error(w, http.StatusInternalServerError, "error creating kubeconfig file for cluster %s (%s)", availableCluster.Name, err)
 			return
 		}
 
 		kubeConfigStr, err := marshalAndEncodeKubeConfig(kubeConfig)
 		if err != nil {
+			log.Printf("error marshaling & encoding kubeconfig (%s)", err)
 			htp.Error(w, http.StatusInternalServerError, "error marshaling & encoding kubeconfig (%s)", err)
 			return
 		}
@@ -147,10 +150,12 @@ func CreateLease(
 		leaseMap.CreateLease(newToken, leases.NewLease(availableCluster.Name, req.ExpirationTime(time.Now())))
 
 		if err := saveAnnotations(services, svc, leaseMap); err != nil {
+			log.Printf("error saving new lease to Kubernetes annotations (%s)", err)
 			htp.Error(w, http.StatusInternalServerError, "error saving new lease to Kubernetes annotations (%s)", err)
 			return
 		}
 		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			log.Printf("error encoding json (%s)", err)
 			htp.Error(w, http.StatusInternalServerError, "error encoding json (%s)", err)
 			return
 		}
--- handlers/delete_lease.go
+++ handlers/delete_lease.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/deis/k8s-claimer/gke"
@@ -30,37 +31,44 @@ func DeleteLease(
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		pathElts := htp.SplitPath(r)
 		if len(pathElts) != 2 {
+			log.Printf("path must be in the format /lease/{token}")
 			htp.Error(w, http.StatusBadRequest, "path must be in the format /lease/{token}")
 			return
 		}
 		leaseToken := uuid.Parse(pathElts[1])
 		if leaseToken == nil {
+			log.Printf("lease token %s is invalid", pathElts[1])
 			htp.Error(w, http.StatusBadRequest, "lease token %s is invalid", pathElts[1])
 			return
 		}
 
 		svc, err := services.Get(k8sServiceName)
 		if err != nil {
+			log.Printf("error getting the %s service (%s)", k8sServiceName, err)
 			htp.Error(w, http.StatusInternalServerError, "error getting the %s service (%s)", k8sServiceName, err)
 			return
 		}
 		leaseMap, err := leases.ParseMapFromAnnotations(svc.Annotations)
 		if err != nil {
+			log.Printf("error getting annotations for the %s service (%s)", k8sServiceName, err)
 			htp.Error(w, http.StatusInternalServerError, "error getting annotations for the %s service (%s)", k8sServiceName, err)
 			return
 		}
 		lease, existed := leaseMap.LeaseForUUID(leaseToken)
 		if !existed {
+			log.Printf("lease %s doesn't exist", leaseToken)
 			htp.Error(w, http.StatusConflict, "lease %s doesn't exist", leaseToken)
 			return
 		}
 		cluster, err := getClusterFromLease(lease, clusterLister, projID, zone)
 		if err != nil {
+			log.Printf("couldn't get cluster from lease (%s)", err)
 			htp.Error(w, http.StatusInternalServerError, "couldn't get cluster from lease (%s)", err)
 			return
 		}
 		cfg, err := createKubeConfigFromCluster(cluster)
 		if err != nil {
+			log.Printf("couldn't create kube config from cluster (%s)", err)
 			htp.Error(w, http.StatusInternalServerError, "couldn't create kube config from cluster (%s)", err)
 			return
 		}
@@ -69,6 +77,7 @@ func DeleteLease(
 		// the lease from annotations, replacing the lease for a cluster with a new UUID anyway
 		deleted := leaseMap.DeleteLease(leaseToken)
 		if !deleted {
+			log.Printf("lease %s doesn't exist", leaseToken)
 			htp.Error(w, http.StatusConflict, "lease %s doesn't exist", leaseToken)
 			return
 		}
@@ -76,16 +85,19 @@ func DeleteLease(
 		if clearNamespaces {
 			namespaces, err := nsFunc(cfg)
 			if err != nil {
+				log.Printf("couldn't create namespaces lister/deleter implementation  (%s)", err)
 				htp.Error(w, http.StatusInternalServerError, "couldn't create namespaces lister/deleter implementation  (%s)", err)
 				return
 			}
 			if err := deleteNamespaces(namespaces, skipDeleteNamespaces); err != nil {
+				log.Printf("error deleting namespaces (%s)", err)
 				htp.Error(w, http.StatusInternalServerError, "error deleting namespaces (%s)", err)
 				return
 			}
 		}
 
 		if err := saveAnnotations(services, svc, leaseMap); err != nil {
+			log.Printf("error saving new annotations (%s)", err)
 			htp.Error(w, http.StatusInternalServerError, "error saving new annotations (%s)", err)
 			return
 		}
